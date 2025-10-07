842b3405fe37961121b2e84ee811c3a05ad7a38a
Deyse <deyse@email.com>
2025-10-07 20:36:29 -0300

    Botão de navegação

--- telas/TelaLogin.jsx
+++ telas/TelaLogin.jsx
@@ -1,9 +1,19 @@
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 function TelaLogin() {
     return (
         <View>
             <Text>Tela de Login</Text>
+
+            <Button
+                title="ir para cadastro"
+                onPress={() => Navigation.navigate('Cadastro')}
+                />
+
+                <Button
+                    title="Ir para Descobrir"
+                    onPress={() => navigation.navigate('Descobrir')}
+                    />
         </View>
     );
 };
